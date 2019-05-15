8e50b429a2eb73eb6d1663bb86032a0664035b8f
Aiden <zzybccdb@gmail.com>
2019-05-15 16:20:44 +0800

    -a

--- Longest_Common_Subsequence_string.cpp
+++ Longest_Common_Subsequence_string.cpp
@@ -1,47 +1,44 @@
 /*
     最长子字串问题 10405 - Longest Common Subsequence
     使用 string
-*/
+    題目大意：從給定的兩個字串中找出它們中最大的公共子子串的長度。
+    解答思考：利用dp的方法解答，建立2維的dp表。
+    首先對於長度爲i，j的兩個子子串，我們會有一個dp【i+1】【j+1】的array
+    如果str1【i】 == str2【j】，那麼dp【i】【j】 = dp【i-1】【j-1】
+    如果str1【i】！= str2【j】，那麼dp【i】【j】 = max（dp【i-1】【j】，dp【i】【j-1】）
+*/ 
 #include<iostream>
 #include<string>
-#include<stdlib.h>
-#include<algorithm>    // std::max
 #include<sstream>
+#include<algorithm>
 using namespace std;
 
-int main(){ 
+int main(){
+    string str1,str2;
     cin >> ws;
-    string str1;
+    // while(getline(cin,str1,'分隔符')){
     while(getline(cin,str1)){
-        stringstream ss1(str1);
-        ss1 >> str1;
-        string str2;
+        cin>>ws;
         getline(cin,str2);
-        stringstream ss2(str2);
-        ss2 >> str2;
-        int l1=str1.length(), l2=str2.length();
-        int dp[l1+1][l2+1];
-        if(l1==0 || l2==0){
-            cout<< 0 <<endl;
-        }
-        else{
-            for(int i=0; i<=l1; i++){
-                for(int j=0; j<=l2; j++){
-                    if(i==0){
-                        dp[i][j]=0;
-                    }
-                    else if(j==0){
-                        dp[i][j]=0;
-                    }
-                    else if(str1[i-1]==str2[j-1]){
-                        dp[i][j]=dp[i-1][j-1]+1;
-                    }
-                    else if(str1[i-1]!=str2[j-1]){
-                        dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                    }
+        int size1 = str1.length(), size2 = str2.length();
+        if(size1 == 0 || size2 == 0)
+            return 0;
+        int dp[size1+1][size2+1];
+        for(int i=0; i <= size1; i++){
+            for(int j=0; j<= size2; j++){
+                if(i==0){
+                    dp[i][j] = 0;
+                }
+                else if(j==0){
+                    dp[i][j] = 0;
+                }
+                else if(str1[i-1] == str2[j-1]){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                }else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
                 }
             }
-            cout << dp[l1][l2]<<endl;
         }
+        cout << dp[size1][size2] << endl;
     }
 }
